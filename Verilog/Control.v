e3d550f27ea2173965b4001f06d06fe364bbf2cb
DCTung <32107842+DCTung@users.noreply.github.com>
2017-11-26 14:15:49 -0800

    Control + fixture

--- Verilog/Control.v
+++ Verilog/Control.v
@@ -1,7 +1,6 @@
 //i'm the control mod
 
 module control(
-  input clk, rst;
   input [3:0] opCode;
   output R15;
   output ALUSrc;
@@ -14,9 +13,9 @@ module control(
   );
 
   always@(*)
-  begin
     case(opcode)
-    4'b1111: //TypeA
+    4'b1111:  //TypeA
+    begin
       R15 = 1;
       ALUSrc = 0;
       MemToReg = 1;
@@ -25,8 +24,10 @@ module control(
       MemWrite = 0;
       Branch = 0;
       ALUOP = 2'b11;
+    end
 
-    4'b1000; //AND imm
+    4'b1000: //AND imm
+    begin
       R15 = 0;
       ALUSrc = 0;
       MemToReg = 1;
@@ -35,8 +36,10 @@ module control(
       MemWrite = 0;
       Branch = 0;
       ALUOP = 2'b11;
+    end
 
-    4'b1001; //OR imm
+    4'b1001: //OR imm
+    begin
       R15 = 0;
       ALUSrc = 1;
       MemToReg = 1;
@@ -45,27 +48,117 @@ module control(
       MemWrite = 0;
       Branch = 0;
       ALUOP = 2'b10;
+    end
 
+      4'b1010:  //LBU
+      begin
+        R15 = 0;
+        ALUSrc = 1;
+        MemToReg = 0;
+        RegWrite = 1;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 0;
+        ALUOP = 2'b10;
+      end
 
+      4'b1011: //SB
+      begin
+        R15 = 0;
+        ALUSrc = 1;
+        MemToReg = 0;
+        RegWrite = 1;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 0;
+        ALUOP = 2'b10;
+      end
 
-/*
-    4'b1000:  //Type C
-      R15 = 0;
-      ALUSrc = 1;
-      MemToReg = 1;
-      RegWrite = 1;
-      MemRead = 0;
-      MemWrite = 0;
-      Branch = 0;
-      ALUOP = 2'b10;
+      4'b1100:  //LB
+      begin
+        R15 = 0;
+        ALUSrc = 1;
+        MemToReg = 0;
+        RegWrite = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 1;
+        ALUOP = 2'b01;
+      end
 
-    4'b1010:  //LBU
-      R15 = 0;
-      ALUSrc = 1;
-      MemToReg = 0;
-      RegWrite = 1;
-      MemRead = 0;
-      MemWrite = 0;
-      Branch = 0;
-      ALUOP = 2'b10;
-    */
+      4'b1101: //Store
+      begin
+        R15 = 0;
+        ALUSrc = 1;
+        MemToReg = X;
+        RegWrite = 1;
+        MemRead = 0;
+        MemWrite = 1;
+        Branch = 0;
+        ALUOP = 2'b00;
+      end
+
+      //Type C
+      4'b0101:  //blt
+      begin
+        R15 = 1;
+        ALUSrc = 1;
+        MemToReg = x;
+        RegWrite = 1;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 1;
+        ALUOP = 2'b01;
+      end
+
+      4'b0100:  //bgt
+      begin
+        R15 = 1;
+        ALUSrc = 1;
+        MemToReg = x;
+        RegWrite = 1;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 1;
+        ALUOP = 2'b01;
+      end
+
+      4'b0110: //blt
+      begin
+        R15 = 1;
+        ALUSrc = 1;
+        MemToReg = x;
+        RegWrite = 1;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 1;
+        ALUOP = 2'b01;
+      end
+
+      //Type D
+      4'b0001:  //jump
+      begin
+        R15 = 0;
+        ALUSrc = 1;
+        MemToReg = 0;
+        RegWrite = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 0;
+        ALUOP = 2'b00;
+      end
+
+      4'b0000:  //jump
+      begin
+        R15 = 0;
+        ALUSrc = 0;
+        MemToReg = 0;
+        RegWrite = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        Branch = 0;
+        ALUOP = 2'b00;
+      end
+endcase
+
+endmodule
---
+++ Verilog/Control_fixture.v
@@ -0,0 +1,54 @@
+`include Control.v
+
+module control_fixture;
+
+reg[3:0] opcode;
+wire R15, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch, ALUOP;
+
+initial
+  $monitor($time, " R15: %b, ALUSrc: %b, MemToReg: %b, RegWRite: %b, MemRead: %b, MemWrite: %b, Branch: %b, ALUOP: %b",
+            R15, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch, ALUOP);
+  control CUT(opcode,R15, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch, ALUOP);
+
+  initial begin
+    opcode = 4'b1111;
+    #5
+    opcode = 4'b1000;
+    #5
+    opcode = 4'b1001;
+    #5
+    opcode = 4'b1010;
+    #5
+    opcode = 4'b1011;
+    #5
+    opcode = 4'b1100;
+    #5
+    opcode = 4'b1101;
+    #5
+    opcode = 4'b0101;
+    #5
+    opcode = 4'b0100;
+    #5
+    opcode = 4'b0110;
+    #5
+    opcode = 4'b0001;
+    #5
+    opcode = 4'b0000;
+
+  initial begin
+      #100 $finish;
+      end
+  endmodule
+
+
+/*
+input [3:0] opCode;
+output R15;
+output ALUSrc;
+output MemToReg;
+output RegWrite;
+output MemRead;
+output MemWrite;
+output Branch;
+output [1:0] ALUOP;
+*/
