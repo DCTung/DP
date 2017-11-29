f90cb66b7a92fe82f852d5066da710e543bac1ec
Sujasen <sujason39@gmail.com>
2017-11-29 10:06:42 -0800

    Update IFID_buffer_fixture.v

--- Verilog/IFID_buffer_fixture.v
+++ Verilog/IFID_buffer_fixture.v
@@ -4,12 +4,13 @@ reg clk, reset;
 reg [15:0] instruc_in;
 reg [7:0] addr_in;
 wire [15:0] instruc_out;
+wire [3:0] opcode;
 wire [7:0] addr_out;
 
 
-initial $monitor ($time, " clk = %b, reset = %b, instruc_in = %h, addr_in = %h, instruc_out = %h, addr_out = %h", clk, reset, instruc_in, addr_in, instruc_out, addr_out);
+	initial $monitor ($time, " clk = %b, reset = %b, instruc_in = %h, addr_in = %h, instruc_out = %h, opcode = %b, addr_out = %h", clk, reset, instruc_in, addr_in, instruc_out, addr_out);
 
-IFID_buffer I1(clk,reset,instruc_in,addr_in, instruc_out, addr_out);
+	IFID_buffer I1(clk,reset,instruc_in,addr_in, instruc_out, opcode, addr_out);
 
 
 initial
