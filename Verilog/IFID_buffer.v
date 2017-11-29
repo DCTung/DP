7b19ac0acee9073c3292a96eb097ca8324da1de9
Sujasen <sujason39@gmail.com>
2017-11-29 13:20:55 -0800

    Update IFID_buffer.v

--- Verilog/IFID_buffer.v
+++ Verilog/IFID_buffer.v
@@ -1,25 +1,14 @@
-module IFID_buffer(
-input clk,reset,  
+module IFID_buffer( 
 input [15:0] instruc_in, 
 input [7:0] addr_in, 
 output reg [15:0] instruc_out,
 output reg [3:0] opcode,
 output reg [7:0] addr_out);
 
-always@(posedge clk or negedge reset)
+always@(*)
 	begin
-		if(reset)
-		begin
-			instruc_out <= 16'h0000;
-			addr_out <= 8'h00;
-			opcode <= 4'h0;
-		end
-
-		else
-		begin
-			instruc_out <= instruc_in;
-			addr_out <= addr_in;
-			opcode <= instuct_in[15:12];
-		end
+		instruc_out = instruc_in;
+		addr_out = addr_in;
+		opcode = instruc_in[15:12];
 	end
 endmodule
