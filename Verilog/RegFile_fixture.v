0724b5b10a75f935e58cd668a4e782187f75511c
Sujasen <sujason39@gmail.com>
2017-11-29 10:07:28 -0800

    Update RegFile_fixture.v

--- Verilog/RegFile_fixture.v
+++ Verilog/RegFile_fixture.v
@@ -3,11 +3,10 @@ module RegFile_fixture;
 	reg clk, reset, RegWrite;
 	reg [15:0] instruc_in, Writedata;
 	wire [15:0] op1, op2;
-	wire [3:0] opcode;
 initial
-	$monitor ($time, " clk = %b, reset = %b, RegWrite = %b, instruc_in = %h, Writedata = %h, op1 = %h, op2 = %h, opcode = %b", clk, reset, RegWrite, instruc_in, Writedata,op1, op2,opcode);
+	$monitor ($time, " clk = %b, reset = %b, RegWrite = %b, instruc_in = %h, Writedata = %h, op1 = %h, op2 = %h", clk, reset, RegWrite, instruc_in, Writedata,op1, op2);
 
-RegFile R1(clk, reset, instruc_in, Writedata,RegWrite, op1, op2,opcode);
+RegFile R1(clk, reset, instruc_in, Writedata,RegWrite, op1, op2);
 
 initial
 	begin 
