ad7a89df23f66adc5cd7d52d523bf48d8393d41d
Sujasen <sujason39@gmail.com>
2017-11-30 20:49:41 -0800

    Update adder.v

--- Verilog/adder.v
+++ Verilog/adder.v
@@ -1,3 +1,8 @@
-module adder(input[7:0] addr_in, input [3:0] constant, output [7:0] addr_out);
-	assign addr_out = addr_in + constant;
+module adder(
+	input[7:0] addr_in, constant, 
+	output reg [7:0] addr_out);
+always@(*)
+	begin
+	addr_out = addr_in + constant;
+	end
 endmodule
