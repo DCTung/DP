585ad5573077dc74a621ce34c4bf917fd13cc74d
Sujasen <sujason39@gmail.com>
2017-12-02 13:29:33 -0800

    Update adder.v

--- Verilog/adder.v
+++ Verilog/adder.v
@@ -1,8 +1,12 @@
 module adder(
+	input clk, reset,
 	input[7:0] addr_in, constant, 
 	output reg [7:0] addr_out);
-always@(*)
-	begin
-	addr_out = addr_in + constant;
-	end
+always@(posedge clk, negedge reset)
+begin
+		if(reset)
+			addr_out <= 8'h00;
+		else 
+		addr_out <= addr_in + constant;
+end
 endmodule
