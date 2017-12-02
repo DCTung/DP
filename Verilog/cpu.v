6b0f37862928078b71555c3c2891858847e78cf1
Sujasen <sujason39@gmail.com>
2017-12-02 15:27:23 -0800

    Update cpu.v

--- Verilog/cpu.v
+++ Verilog/cpu.v
@@ -2,7 +2,8 @@
 `include "adder.v"
 `include "IM.v"
 `include "IFID_buffer.v"
-//'include "control.v"
+`include "control.v"
+`include "RegFile.v"
 
 module cpu(input clk, reset);
 //PC signals
@@ -21,19 +22,27 @@ wire [3:0] opcode, funct;
 wire [11:0] offset;
 
 //control
-//reg[3:0] opcode;
-//wire R15, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch;
-//wire[1:0] ALUOP;
+reg[3:0] con_opcode;
+wire R15, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch;
+wire[1:0] ALUOP;
+
+//RegFile
+reg [15:0] Writedata;
+reg [3:0] Writereg;
+wire [15:0] op1, op2, Reg15;
 
 			adder         add1(.clk(clk), .reset(reset), .addr_in(addr_out), .constant(constant), .addr_out(addr_out));
 			PC             pc1( .addr_in(add1.addr_out), .addr_out(addr_out));
 			IM             im1(.reset(reset), .addr_in(pc1.addr_out), .addr_out(addr_out), .instruc_out(instruc_out));
 			IFID_buffer   IFID(.instruc_in(im1.instruc_out), .addr_in(im1.addr_out),.instruc_out(instruc_out), .opcode(opcode), .funct(funct), .addr_out(addr_out), .offset(offset));
-
-initial	$monitor(" \n PC: reset = %b, addr_in = %h, addr_out = %h \n   adder: addr_in = %h, constant = %h, addr_out = %h, \n   IM: addr_in = %h, addr_out = %h, instruct_out = %h, \n   IFID: instruc_in = %h, addr_in = %h, instruc_out = %h, opcode = %b, funct = %b, addr_out = %h, offset = %b\n", 
+			control 	c1(.clk(clk), .reset(reset), .con_opcode(IFID.opcode), .R15(R15), .ALUSrc(ALUSrc), .MemToReg(MemToReg), .RegWrite(RegWrite), .MemRead(MemRead), .MemWrite(MemWrite), .Branch(Branch), .ALUOP(ALUOP));
+			RegFile		r1(.instruc_in(IFID.instruc_out), .Writedata(Writedata), .Writereg(Writereg), .RegWrite(c1.RegWrite), .reset(reset), .op1(op1), .op2(op2), .Reg15(Reg15));
+initial	$monitor(" \n    PC: reset = %b, addr_in = %h, addr_out = %h \n   ADDER: addr_in = %h, constant = %h, addr_out = %h, \n   IM: addr_in = %h, addr_out = %h, instruct_out = %h, \n   IFID: instruc_in = %h, addr_in = %h, instruc_out = %h, opcode = %b, funct = %b, addr_out = %h, offset = %b\n   CONTROL: opcode = %b, R15 = %b, ALUSrc = %b, MemtoReg = %b, RegWrite = %b, MemRead = %b, MemWrite = %b, Branch = %b, ALUOP = %b\n   REGFILE: instruc_in = %h, WriteData = %h, WriteReg = %d, RegWrite = %b, op1 = %h, op2 = %h, Reg15 = %h", 
 reset, addr_in, addr_out,
-PC.addr_out, add1.constant, addr_out,
-PC.addr_out, addr_out, instruc_out,
-IM.instruc_out,IM.addr_out, instruc_out, opcode, funct, addr_out, offset);
+pc1.addr_out, add1.constant, addr_out,
+pc1.addr_out, addr_out, instruc_out,
+im1.instruc_out, im1.addr_out, instruc_out, opcode, funct, addr_out, offset,
+IFID.opcode, R15, ALUSrc, MemToReg, RegWrite, MemRead,MemWrite, Branch, ALUOP,
+IFID.instruc_out, Writedata, Writereg, c1.RegWrite, op1, op2, Reg15);
 
 endmodule 
