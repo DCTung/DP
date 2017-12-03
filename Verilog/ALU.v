fa50928829b879b1a46a30886095c9feafdac707
Sujasen <sujason39@gmail.com>
2017-12-03 13:28:19 -0800

    Update ALU.v

--- Verilog/ALU.v
+++ Verilog/ALU.v
@@ -1,6 +1,6 @@
 module ALU(
-  input clk, rst,
-  input [3:0] functCode,
+  input clk, reset,
+  input [3:0] operation,
   input signed [15:0] op1, op2,
   output reg signed [15:0] result,
   output reg [15:0] remainder,
@@ -8,42 +8,41 @@ module ALU(
   //need other condits
   );
 
-  always@(posedge clk, negedge rst)
-  begin
-  if(!rst)
-    begin
-    result <= 0;
-    remainder <=0;
-    o <= 0;
+  always@(posedge clk, negedge reset)
+  	begin
+  		if(reset)
+    			begin
+    				result <= 0;
+    				remainder <=0;
+    				o <= 0;
+    			end
+    		else
+    		case(operation)
+      			4'b0001:
+        			result <= op1 + op2;    // ADD
+      			4'b0010: //sub
+				result <= op1 - op2;    // sub
+      			4'b0011: 
+      			begin
+        			result <= op1 * op2;    //mult
+        			remainder <= op1%op2;   
+      			end
+      			4'b0100: 			//divvies
+      			begin
+        			result <= op1/op2;
+        			remainder <= op1%op2;
+      			end
+      			4'b0101: 			//move
+        			result <= op1;
+      			4'b0110: 			//swappies
+        			result <= op1;
+       				//op2 straight to buff?
+			4'b0111:
+				result <= op1 & op2;
+			4'b1000:
+				result <= op1 | op2;
+			4'b1001:
+				result	<= op1 ^ op2;
+      		endcase
     end
-    else
-    case(functCode)
-      4'b0000: //addies
-        result <= op1 + op2;
-        //result <= temp[15:0];
-        //need overflow detection for each @@@@
-      4'b0001: //subbies
-     //  begin
-	result <= op1 - op2;
-        //result <= temp[15:0];
-     // end
-      4'b0100: //multiplic
-      begin
-        result <= op1 * op2;
-        remainder <= op1%op2;
-       // result <= temp[15:0];
-      end
-      4'b0101: //divvies
-      begin
-        result <= op1/op2;
-        remainder <= op1%op2;
-      end
-      4'b0111: //move
-        result = op1 + 0;
-
-      4'b1000: //swappies
-        result = op1;
-        //op2 straight to buff?
-      endcase
-    end
-  endmodule
+endmodule
