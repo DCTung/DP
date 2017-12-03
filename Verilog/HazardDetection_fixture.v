4bccb6b7482d0f9ffe8f7c5af2b5f01e968fb361
Sujasen <sujason39@gmail.com>
2017-12-02 17:36:06 -0800

    Update HazardDetection_fixture.v

--- Verilog/HazardDetection_fixture.v
+++ Verilog/HazardDetection_fixture.v
@@ -12,17 +12,22 @@ $monitor($time, "\n INPUT:\n IFID_op1: %h IFID_op2: %h IDEX_op1: %h \n IDEX_MemR
 HazardDetection HDF(IFID_op1, IFID_op2, IDEX_op1, IDEX_MemRead, rst, STALL, PCWrite, IFID_Write);
 
 initial begin
+rst = 1;
 #10
+rst = 0;
+IDEX_MemRead = 1;
 IFID_op1 = 12;
 IFID_op2 = 13;
 IDEX_op1 = 12;
 
 #10
+IDEX_MemRead = 1;
 IFID_op1 = 15;
 IFID_op2 = 10;
 IDEX_op1 = 3;
 
 #10
+IDEX_MemRead = 1;
 IFID_op1 = 4;
 IFID_op2 = 5;
 IDEX_op1 = 5;
