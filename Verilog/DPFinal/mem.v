bbc390c2ad125382a542a0916a008fd70b881bfb
DCTung <32107842+DCTung@users.noreply.github.com>
2017-12-05 16:18:19 -0800

    MUXALUOP2

--- Verilog/DPFinal/mem.v
+++ Verilog/DPFinal/mem.v
@@ -10,8 +10,8 @@ always@(posedge clk or negedge reset)
 	begin
 		data_out <= 16'h0000;
 		m[8'h00] <= 16'h2BCD;
-		m[8'h02] <= 16'h0000; 
-		m[8'h04] <= 16'h1234; 
+		m[8'h02] <= 16'h0000;
+		m[8'h04] <= 16'h1234;
 		m[8'h06] <= 16'hDEAD;
 		m[8'h08] <= 16'hBEEF;
 		for(i = 8'h10; i < 255; i = i + 1)
@@ -20,14 +20,13 @@ always@(posedge clk or negedge reset)
 	else if(MemRead && MemWrite)
 	begin
 		data_out <= m[Memaddr_in];
-		m[Memaddr_in] <= data_in; 
-	end	
+		m[Memaddr_in] <= data_in;
+	end
 	else if(MemRead && !MemWrite)
-		data_out <= m[Memaddr_in];	
+		data_out <= m[Memaddr_in];
 	else if(!MemRead && MemWrite)
-		m[Memaddr_in] <= data_in; 
+		m[Memaddr_in] <= data_in;
 endmodule
-		
-		
 
-		
+
+//Contorl signal will tell us if we're loading/storing -- SE signal will be muxed in so just parse the ALUResult into the Mem
