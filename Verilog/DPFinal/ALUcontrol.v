adcd78a1f92eacd2d7c8a370808aa81db714205d
Sujasen <sujason39@gmail.com>
2017-12-05 17:33:19 -0800

    Update ALUcontrol.v

--- Verilog/DPFinal/ALUcontrol.v
+++ Verilog/DPFinal/ALUcontrol.v
@@ -3,18 +3,17 @@ module ALUcontrol(
 		input [3:0] funct,
 		input [1:0] ALUop,
 		output reg [3:0] operation );
-always@(posedge clk, negedge reset)
+always@(*)
 	//if(reset)
 	//	operation <= 4'h1;
 	//else
 		begin
 			case(ALUop)
 				2'b11:
-				//begin
 					case(funct)
 					4'b0000:
 						operation <= 4'b0001;  //add
-					4'b0010:
+					4'b0001:
 						operation <= 4'b0010;  //sub
 					4'b0100:
 						operation <= 4'b0011;  //mult
@@ -24,14 +23,11 @@ always@(posedge clk, negedge reset)
 						operation <= 4'b0101;  //move
 					4'b1000:
 						operation <= 4'b0110;  //swap
-					4'b1010:
+					4'b1001:
 						operation <= 4'b0111;  //AND gate
-					4'b1011:
+					4'b1010:
 						operation <= 4'b1000;  //OR gate
-				//	default:
-					//	operation <= 4'b0001;  //will never occur
 					endcase
-				//end
 				2'b10:
 					operation <= 4'b0001;	      //load & store (add)
 				2'b01:
