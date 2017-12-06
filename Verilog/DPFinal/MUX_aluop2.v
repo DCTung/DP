fe7c868c3144b2f5fbc356a6135ed27e1cd8a9b9
DCTung <32107842+DCTung@users.noreply.github.com>
2017-12-05 16:28:52 -0800

    AL-YOUUUU

--- Verilog/DPFinal/MUX_aluop2.v
+++ Verilog/DPFinal/MUX_aluop2.v
@@ -1,11 +1,11 @@
 module MUX_aluop2(
   input [15:0] SE, FB_MUX_Result,
-  input [3:0] opcode,
-  output reg mux_ALUOP2_out
+  input ALUSRC,
+  output reg [15:0] mux_ALUOP2_out
   );
 
   always@(*)
-    if (opcode == 4'b1010 || opcode == 4'b1011 || opcode == 4'b1100 || opcode == 4'b1101)
+    if (ALUSRC)
       mux_ALUOP2_out = SE;
     else
       mux_ALUOP2_out = FB_MUX_Result;
